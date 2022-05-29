6322ab603f573b5ab49001c0587b10d7d0d0a10a
Ken Leung <kenlhk01@gmail.com>
2022-05-29 14:10:31 +0100

    add Find All Numbers Disappeared solution

---
+++ array/find_all_numbers_disappered/solution.py
@@ -0,0 +1,15 @@
+"""
+Given an array nums of n integers where nums[i] is in the range [1, n],
+return an array of all the integers in the range [1, n] that do not appear in nums.
+"""
+
+
+def findDisappearedNumbers(nums):
+    for i in range(len(nums)):
+        index = abs(nums[i]) - 1
+        nums[index] = -abs(nums[index])
+    return [i + 1 for i in range(len(nums)) if nums[i] > 0]
+
+
+print(findDisappearedNumbers([1, 1]))
+print(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
